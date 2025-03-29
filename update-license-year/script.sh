5646a492f43292901582ef8833b8b3c118788165
Eduarda Bastos <eduardasofia2000@gmail.com>
2025-03-29 16:54:53 +0000

    Update script

--- update-license-year/script.sh
+++ update-license-year/script.sh
@@ -4,12 +4,19 @@ CURRENT_YEAR=$(date +"%Y")
 
 if [ -f "LICENSE" ]; then
     sed -i "s/[0-9]\{4\}/$CURRENT_YEAR/g" LICENSE
+    
     git config --global user.name "github-actions[bot]"
     git config --global user.email "github-actions[bot]@users.noreply.github.com"
 
     git remote set-url origin https://x-access-token:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git
 
     git add LICENSE
-    git commit -m "Update license year to $CURRENT_YEAR" || echo "No changes to commit"
-    git push
+    if git diff --cached --quiet; then
+        echo "No changes to commit"
+    else
+        git commit -m "Update license year to $CURRENT_YEAR"
+        git push
+    fi
+else
+    echo "LICENSE file not found"
 fi
